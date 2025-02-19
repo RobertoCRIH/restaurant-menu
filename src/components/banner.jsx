828234579e7d1abd04c2a4b2261e58eecb6bdb33
RobertoCRIH <108534863+RobertoCRIH@users.noreply.github.com>
2025-02-19 16:10:10 -0600

    Primer commit. Banner terminado.

---
+++ src/components/banner.jsx
@@ -0,0 +1,21 @@
+import bannerImg from "../images/banner.jpg"
+import './banner.scss'
+
+function Banner() {
+    return(
+        <div className="banner">
+           
+            <div className="banner__content">
+                <div className="text1">
+                    CHECA NUESTRO
+                </div>
+                <br />
+                <div className="text2">
+                    MENÚ
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
